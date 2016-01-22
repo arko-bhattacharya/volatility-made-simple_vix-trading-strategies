e8db5e1eb96a7799ff38c06e5d37e9a91c3d44ec
arko-bhattacharya <arko.bhattacharya@gmail.com>
2016-01-22 18:17:20 +0530

    Changing the return data format from being a named num to a dataframe with columns for buy date, sell date and returns.

--- src/strategy_VIX-VXVratio.R
+++ src/strategy_VIX-VXVratio.R
@@ -1,16 +1,19 @@
+## For strategy details refer to
+## http://volatilitymadesimple.com/vix-trading-strategies-in-june/
+##  
 ## > load('../data/main_data.RData')
 ## >
 ## > head(data)
-##     UX1   UX2  UX_CM1M   vix     snp    xiv    vxx       date
-## 1 23.20 25.80 23.89333 23.54 1180.55  9.557 788.64 2010-11-30
-## 2 21.90 24.75 22.75500 21.36 1206.07  9.787 751.20 2010-12-01
-## 3 19.85 23.00 20.90000 19.39 1221.53 10.429 692.96 2010-12-02
-## 4 19.05 22.15 20.18667 18.01 1224.71 11.041 660.80 2010-12-03
-## 5 18.60 21.60 20.00000 18.02 1223.12 11.252 648.80 2010-12-06
-## 6 18.60 21.45 20.02500 17.99 1223.75 11.358 642.40 2010-12-07
-## >
+##     UX1   UX2  UX_CM1M   vix     snp    xiv    vxx   vxv      ziv   vxz       date
+## 1 23.20 25.80 23.89333 23.54 1180.55  9.557 788.64 25.19 12.29875 74.56 2010-11-30
+## 2 21.90 24.75 22.75500 21.36 1206.07  9.787 751.20 23.99 12.45500 72.57 2010-12-01
+## 3 19.85 23.00 20.90000 19.39 1221.53 10.429 692.96 21.99 12.45500 70.13 2010-12-02
+## 4 19.05 22.15 20.18667 18.01 1224.71 11.041 660.80 21.00 13.09500 68.63 2010-12-03
+## 5 18.60 21.60 20.00000 18.02 1223.12 11.252 648.80 21.04 13.09500 68.02 2010-12-06 
+## 6 18.60 21.45 20.02500 17.99 1223.75 11.358 642.40 20.97 13.31875 67.50 2010-12-07
+##
 ## > str(data)
-## 'data.frame':	1162 obs. of  8 variables:
+## 'data.frame':	1162 obs. of  11 variables:
 ##  $ UX1    : num  23.2 21.9 19.9 19.1 18.6 ...
 ##  $ UX2    : num  25.8 24.8 23 22.1 21.6 ...
 ##  $ UX_CM1M: num  23.9 22.8 20.9 20.2 20 ...
@@ -18,8 +21,11 @@
 ##  $ snp    : num  1181 1206 1222 1225 1223 ...
 ##  $ xiv    : num  9.56 9.79 10.43 11.04 11.25 ...
 ##  $ vxx    : num  789 751 693 661 649 ...
+##  $ vxv    : num  25.2 24 22 21 21 ...
+##  $ ziv    : num  12.3 12.5 12.5 13.1 13.1 ...
+##  $ vxz    : num  74.6 72.6 70.1 68.6 68 ...
 ##  $ date   : POSIXct, format: "2010-11-30" "2010-12-01" ...
-## > 
+## >
 strategy <- function(data)
   {
     ratio <- data$vix/data$vxv
@@ -56,13 +62,18 @@ strategy <- function(data)
 
                                         # returns
     returns_xiv <- ((sell_price_xiv - buy_price_xiv)/ buy_price_xiv)
-    names(returns_xiv) <- buy_xiv_dates
+    returns_xiv <- data.frame(returns = returns_xiv,
+                              buy_date = as.POSIXct(buy_xiv_dates),
+                              sell_date = as.POSIXct(sell_xiv_dates),
+                              stringsAsFactors = FALSE)    
     
     returns_vxx <- ((sell_price_vxx - buy_price_vxx)/ buy_price_vxx)
-    names(returns_vxx) <- buy_vxx_dates
+    returns_vxx <- data.frame(returns = returns_vxx,
+                              buy_date = as.POSIXct(buy_vxx_dates),
+                              sell_date = as.POSIXct(sell_vxx_dates),
+                              stringsAsFactors = FALSE)
     
-    ret_val <- c(returns_xiv, returns_vxx)
-    ret_val <- ret_val[order(as.POSIXct(names(ret_val)))]
-
+    ret_val <- rbind(returns_xiv, returns_vxx)
+    ret_val <- ret_val[order(ret_val$buy_date),]
     return(ret_val)
   }
--- src/strategy_short-ux-cm1m.R
+++ src/strategy_short-ux-cm1m.R
@@ -1,25 +1,36 @@
+## For strategy details refer to
+## http://volatilitymadesimple.com/vix-trading-strategies-in-june/
+##  
+## > load('../data/main_data.RData')
 ## >
 ## > head(data)
-##       UX_CM1M    UX1    UX2       date
-## 2231 15.21333 14.150 15.600 2015-07-14
-## 2230 15.24500 14.475 15.575 2015-07-13
-## 2229 17.01500 16.775 17.175 2015-07-10
-## 2228 18.74000 18.825 18.675 2015-07-09
-## 2227 18.29167 18.425 18.175 2015-07-08
-## 2226 16.50000 16.325 16.675 2015-07-07
-## >
+##     UX1   UX2  UX_CM1M   vix     snp    xiv    vxx   vxv      ziv   vxz       date
+## 1 23.20 25.80 23.89333 23.54 1180.55  9.557 788.64 25.19 12.29875 74.56 2010-11-30
+## 2 21.90 24.75 22.75500 21.36 1206.07  9.787 751.20 23.99 12.45500 72.57 2010-12-01
+## 3 19.85 23.00 20.90000 19.39 1221.53 10.429 692.96 21.99 12.45500 70.13 2010-12-02
+## 4 19.05 22.15 20.18667 18.01 1224.71 11.041 660.80 21.00 13.09500 68.63 2010-12-03
+## 5 18.60 21.60 20.00000 18.02 1223.12 11.252 648.80 21.04 13.09500 68.02 2010-12-06 
+## 6 18.60 21.45 20.02500 17.99 1223.75 11.358 642.40 20.97 13.31875 67.50 2010-12-07
+##
 ## > str(data)
-## 'data.frame':	2231 obs. of  4 variables:
-##  $ UX_CM1M: num  15.2 15.2 17 18.7 18.3 ...
-##  $ UX1    : num  14.2 14.5 16.8 18.8 18.4 ...
-##  $ UX2    : num  15.6 15.6 17.2 18.7 18.2 ...
-##  $ date   : POSIXct, format: "2015-07-14" "2015-07-13" ...
+## 'data.frame':	1162 obs. of  11 variables:
+##  $ UX1    : num  23.2 21.9 19.9 19.1 18.6 ...
+##  $ UX2    : num  25.8 24.8 23 22.1 21.6 ...
+##  $ UX_CM1M: num  23.9 22.8 20.9 20.2 20 ...
+##  $ vix    : num  23.5 21.4 19.4 18 18 ...
+##  $ snp    : num  1181 1206 1222 1225 1223 ...
+##  $ xiv    : num  9.56 9.79 10.43 11.04 11.25 ...
+##  $ vxx    : num  789 751 693 661 649 ...
+##  $ vxv    : num  25.2 24 22 21 21 ...
+##  $ ziv    : num  12.3 12.5 12.5 13.1 13.1 ...
+##  $ vxz    : num  74.6 72.6 70.1 68.6 68 ...
+##  $ date   : POSIXct, format: "2010-11-30" "2010-12-01" ...
 ## >
 rm(list = ls())
 strategy <- function(data)
   {
-    buy_date <- data$date[1:(norw(data) - 1)]
-    sell_date <- data$date[2:norw(data)]
+    buy_date <- data$date[1:(nrow(data) - 1)]
+    sell_date <- data$date[2:nrow(data)]
 
     buy_price <- data$UX_CM1M[which(data$date %in% buy_date)]
     sell_price <- data$UX_CM1M[which(data$date %in% sell_date)]
@@ -29,6 +40,7 @@ strategy <- function(data)
     
     ret_val <- (((sell_price*sell_quantity) - (buy_price*buy_quantity))/
                 (buy_price*buy_quantity))
-    names(ret_val) <- sell_date
+    ret_val <- data.frame(returns = ret_val, buy_date = buy_date, sell_date = sell_date,
+                          stringsAsFactors = FALSE)
     return(ret_val)
   }
