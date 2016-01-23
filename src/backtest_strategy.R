c02d30b18702a987d4f23c094dc5588ed5e96109
arko-bhattacharya <arko.bhattacharya@gmail.com>
2016-01-24 02:33:43 +0530

    Added comments to explain how to run this script and simlpfied the script.

--- src/backtest_strategy.R
+++ src/backtest_strategy.R
@@ -1,6 +1,18 @@
 ## This is the script that runs the strategies.
 ## Every strategy is a function which takes in data and gives us an
 ## object which shows us the returns generated using that strategy.
+##
+## This script is designed for execution from command line.
+## The inputs to this script are the strategy script with its path and
+## the output file name with its desired storage location.
+## Example: If one wants to run ./src/strategy_rsi2.R, then one should
+## set the working directory to ./src and then type the following
+## command -
+## Rscript backtest_strategy.R ./strategy_rsi2.R ../strategy_results/rsi2.RData
+## i.e
+## Rscript [file_path]/backtest_strategy.R [file_path]/[strategy name] [file path]/[desired
+## name for storing strategy result]
+
 #!/usr/bin/env Rscript
 rm(list = ls())
 
@@ -12,21 +24,12 @@ cargs <- commandArgs(trailingOnly = TRUE)
 if (length(cargs) != 2) {
   print(cat(usage))
 } else {
-  file_path <- cargs[[1]]
+  ip_file_path <- cargs[[1]]
+  op_file_path <- cargs[[2]]
 }
 
 ## read a strategy
-source(file_path)
-
-## record strategy name
-strategy_name <- unlist(lapply(X = strsplit(x = file_path, split = '/'),
-                               FUN = function(k) {
-                                 ret_val <- lapply(X = strsplit(x = k[2], split = '_'),
-                                                   FUN = function(m) return(
-                                                     substr(x = m[2], start = 1,
-                                                            stop = nchar(m[2]) - 4)))
-                                 
-                               }))
+source(ip_file_path)
 
 ## load data
 load('../data/main_data.RData')
@@ -35,5 +38,6 @@ load('../data/main_data.RData')
 result <- strategy(data = data)
 
 ## store result
-save(result, file = paste('../strategy_results/', strategy_name, '.RData', sep = ''))
+op_file_path <- gsub(pattern = 'NULL', replacement = '', x = op_file_path)
+save(result, file = op_file_path)
 print('Back test completed')
